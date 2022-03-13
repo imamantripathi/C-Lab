f546f1f1cd195a9cf6e99a4b1c3e9f881ae23f65
Aman Tripathi <imamantripathi@gmail.com>
2022-03-13 11:33:17 +0530

    Practical 41

--- Practical_41.c
+++ Practical_41.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
-#include<conio.h>
-void swap(int *x,int *y)
+#include <stdio.h>
+#include <conio.h>
+void swap(int *x, int *y)
 {
-	int temp;
-	temp=*x;
-	*x=*y;
-	*y=temp;
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
 }
 int main()
 {
-	int a,b;
-	printf("\nEnter first number: ");
-	scanf("%d",&a);
-	printf("\nEnter second number: ");
-	scanf("%d",&b);
-	printf("\nBefore swapping, value of a: %d and value of b: %d",a,b);
-	swap(&a,&b);
-	printf("\nAfter swapping, value of a: %d and value of b: %d",a,b);
-	getch();
-	return 0;
+    int a, b;
+    printf("\nEnter first number: ");
+    scanf("%d", &a);
+    printf("\nEnter second number: ");
+    scanf("%d", &b);
+    printf("\nBefore swapping, value of a: %d and value of b: %d", a, b);
+    swap(&a, &b);
+    printf("\nAfter swapping, value of a: %d and value of b: %d", a, b);
+    getch();
+    return 0;
 }
